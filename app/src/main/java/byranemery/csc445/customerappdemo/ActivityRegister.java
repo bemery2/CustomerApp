ec33c8752cd69a131740df288b43a6cfce383c53
bemery2 <bemery2@missouriwestern.edu>
2018-03-18 15:48:26 -0500

    :P

--- app/src/main/java/byranemery/csc445/customerappdemo/ActivityRegister.java
+++ app/src/main/java/byranemery/csc445/customerappdemo/ActivityRegister.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.spothero.emailvalidator.EmailValidationResult;
+import com.spothero.emailvalidator.EmailValidator;
 
 public class ActivityRegister extends AppCompatActivity implements View.OnClickListener {
     private EditText fName, lName, address, aptNum, stateAbb, zipCode, phoneNum, email, password, cpassword;
@@ -17,10 +21,27 @@ public class ActivityRegister extends AppCompatActivity implements View.OnClickL
         address = findViewById(R.id.addressInput);
         aptNum = findViewById(R.id.apartmentNumInput);
         stateAbb = findViewById(R.id.stateAbbrev);
+        zipCode = findViewById(R.id.zipCodeInput);
+        phoneNum = findViewById(R.id.phoneNumberInput);
+        email = findViewById(R.id.emailInput);
+        password = findViewById(R.id.passwordInput);
+        cpassword = findViewById(R.id.cpasswordInput);
     }
 
+    public boolean validateEmail(){
+        EmailValidationResult.ValidationError error = EmailValidator.validateSyntax(email.getText().toString());
+        if (error != null) {
+            email.setError(error.toString());
+            return false;
+        } else
+            return true;
+    }
     @Override
     public void onClick(View v) {
-
+        if (v.getId() == R.id.submit_button){
+            if (validateEmail() && password.getText().toString().equals(cpassword.getText().toString())){
+                Toast.makeText(this, "successful", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
---
+++ app/src/main/java/byranemery/csc445/customerappdemo/ActivityRestaurant.java
@@ -0,0 +1,16 @@
+package byranemery.csc445.customerappdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Siren10 on 18-Mar-18.
+ */
+
+public class ActivityRestaurant extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
